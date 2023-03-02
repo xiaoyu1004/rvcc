f87d7fd26a3641893b0288306b244fa92f95dba9
hongzhang.yu <15216142871@163.com>
2023-03-02 22:13:58 +0800

    support +- operator

--- build/tmp.s
+++ build/tmp.s
@@ -1,4 +1,6 @@
     .global main
 main:
-    li a0, 42
+    li a0, 16
+    addi a0, a0, -2
+    addi a0, a0, 4
     ret
--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, const char **argv)
+int main(int argc, char **argv)
 {
     if (argc != 2)
     {
@@ -9,12 +9,33 @@ int main(int argc, const char **argv)
         return -1;
     }
 
+    char *p = argv[1];
+
     // declare a global main segment, it is also the start of program
     printf("    .global main\n");
     // main label
     printf("main:\n");
     // li is the addi alias instruction, Load an immediate count to the register
-    printf("    li a0, %d\n", atoi(argv[1]));
+    printf("    li a0, %ld\n", strtol(p, &p, 10));
+
+    while (*p)
+    {
+        if (*p == '+')
+        {
+            p++;
+            printf("    addi a0, a0, %ld\n", strtol(p, &p, 10));
+        }
+        else if (*p == '-')
+        {
+            p++;
+            printf("    addi a0, a0, -%ld\n", strtol(p, &p, 10));
+        }
+        else
+        {
+            fprintf(stderr, "unexpected character: '%c'\n", *p);
+        }
+    }
+
     // ret is the jalr x0, x1, 0 alias instruction, Used to return a subroutine
     printf("    ret\n");
 }
